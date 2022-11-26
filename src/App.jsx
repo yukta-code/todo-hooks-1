9cc8c644589b21bbb374e2d95cbf1f9713e7ca87
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-26 19:01:39 +0100

    Start work converting to Vite

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { createContext } from "react";
+import { useImmerReducer } from "use-immer";
+import { Table, Row, Col } from "antd";
+
+import { AddTodoForm } from "./components/Form/AddTodoForm.component";
+import { todoReducer } from "./utils/functions/formReducer";
+
+import { FORM_INITIAL_STATE } from "./utils/constants/INITIAL_STATE";
+import { FORM_COLUMNS } from "./utils/constants/FORM_COLUMNS";
+
+export const TodoContext = createContext();
+
+const App = () => {
+  const [todos, dispatchTodos] = useImmerReducer(
+    todoReducer,
+    FORM_INITIAL_STATE
+  );
+  return (
+    <TodoContext.Provider value={[todos, dispatchTodos]}>
+      <Row type="flex" justify="center">
+        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+          <Table dataSource={todos} columns={FORM_COLUMNS} />
+        </Col>
+      </Row>
+      <Row type="flex" justify="center">
+        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+          <AddTodoForm />
+        </Col>
+      </Row>
+    </TodoContext.Provider>
+  );
+};
+
+export default App;
---
+++ src/utils/constants/FORM_COLUMNS.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Complete } from "../../components/Complete/Complete.component";
+import { Delete } from "../../components/Delete/Delete.component";
+import { Title } from "../../components/Title/Title.component";
+
+export const FORM_COLUMNS = [
+  {
+    title: "Title",
+    dataIndex: "title",
+    key: "title",
+    filters: [
+      {
+        text: "Hide Completed",
+        value: "hide",
+      },
+    ],
+    onFilter: (_, record) => record.completed === "false",
+    render: (text, record) => <Title record={record}>{text}</Title>,
+  },
+  {
+    title: "Date",
+    dataIndex: "date",
+    key: "date",
+    render: (text, record) => <Title record={record}>{text}</Title>,
+  },
+  {
+    title: "Action",
+    key: "action",
+    dataIndex: "action",
+    render: (_, record) => (
+      <>
+        <Complete record={record} />
+        <Delete record={record} />
+      </>
+    ),
+  },
+];
